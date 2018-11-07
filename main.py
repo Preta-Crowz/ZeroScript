487fc3ba86c7e37237f3692d6ccaba4fb17dedd8
Preta Crowz <pretacrowz@gmail.com>
2018-11-07 16:12:46 +0900

    Initial commit

---
+++ main.py
@@ -0,0 +1,42 @@
+import zclass
+func = {}
+var = {}
+curr = []
+code = []
+script = ""
+startpoint = {"[":"list","(":"tuple","<":"set","{":"dict"}
+endpoint = {"list":"]","tuple":")","set":">","dict":"}","byte":"\'","string":"\""}
+
+
+
+if __name__ == "__main__":
+    while True:
+        tag = ">>> "
+        if curr != []: tag = "... "
+        raw = input(tag)
+        for char in raw:
+            if char == ";" and curr == []:
+                code.append(script)
+                script = ""
+                continue
+            if char in endpoint.values():
+                if len(curr) and endpoint[curr[-1]] == char:
+                    curr.pop(-1)
+                    continue
+            if char in startpoint:
+                curr.append(startpoint[char])
+                continue
+            if char == "\'":
+                if not len(curr) or (len(curr) and curr[-1] != "byte"):
+                    curr.append("byte")
+                    continue
+            if char == "\"":
+                if not len(curr) or (len(curr) and curr[-1] != "string"):
+                    curr.append("string")
+                    continue
+            script += char
+        if curr != []: continue
+        code.append(script)
+        script = ""
+        print(code)
+        code = []
---
+++ zclass.py
@@ -0,0 +1,39 @@
+class zstr:
+    def __init__(self,value):
+        self.value = value
+        self._python = value
+
+class zbyte:
+    def __init__(self,value):
+        self.value = value
+        self._python = value
+
+class zint:
+    def __init__(self,value):
+        self.value = value
+        self._python = value
+
+class zfloat:
+    def __init__(self,value):
+        self.value = values
+        self._python = values
+
+class zlist:
+    def __init__(self,*values):
+        self.value = values
+        self._python = values
+
+class zset:
+    def __init__(self,*values):
+        self.value = values
+        self._python = values
+
+class ztuple:
+    def __init__(self,*values):
+        self.value = values
+        self._python = values
+
+class zdict:
+    def __init__(self,*values):
+        self.value = values
+        self._python = values
