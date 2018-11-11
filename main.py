92a026e5c32901d26d8aae1624e7ef09c0ad14f3
Preta Crowz <pretacrowz@gmail.com>
2018-11-11 14:24:54 +0900

    기본 값만 일단 넣어둠

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
 import re
+import zclass
+func = {}
+var = {}
+curr = []
+code = []
+script = ""
+startpoint = {"[":"list","(":"tuple","<":"set","{":"dict"}
+endpoint = {"list":"]","tuple":")","set":">","dict":"}","byte":"\'","string":"\""}
