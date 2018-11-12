5eec0aba585a53d809c4538fc8d2c4f364be174b
Preta Crowz <pretacrowz@gmail.com>
2018-11-12 16:44:07 +0900

    초안 짜두고 이제 정규식 짜야..

--- main.py
+++ main.py
@@ -7,3 +7,10 @@ code = []
 script = ""
 startpoint = {"[":"list","(":"tuple","<":"set","{":"dict"}
 endpoint = {"list":"]","tuple":")","set":">","dict":"}","byte":"\'","string":"\""}
+
+'''
+var name = value
+func name = (input) {
+   code 
+}
+'''
