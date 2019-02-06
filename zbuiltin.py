97f7d721e45289f9080b98fce034370109f8f539
Preta Crowz <pretacrowz@gmail.com>
2019-02-07 03:33:09 +0900

    modified log / variable set / call function and added input

--- zbuiltin.py
+++ zbuiltin.py
@@ -3,7 +3,10 @@ from zclass import *
 func = {}
 var = {}
 
-def log(value): print(do(value).value)
+def log(value):
+    v = do(value).value
+    print(v)
+    return v
 func["log"] = log
 
 func["end"] = exit
@@ -11,6 +14,10 @@ func["exit"] = exit
 func["quit"] = exit
 func["close"] = exit
 func["close_the_console_and_zeroscript_process_from_my_system"] = exit
+
+def inp(text=None):
+    return zstr(input(text.value) if text is not None else input())
+func["input"] = inp
 
 def do(script,**kargs):
     global func,var
@@ -27,8 +34,8 @@ def do(script,**kargs):
     for c in script:
         if kargs.get("debug",False): print(c)
         if c == "": continue
-        elif re.match("^var +[^ ]+ *= *\".*\"$",c):
-            m = re.match("^var +(?P<varname>[^ ]+) *= *(?P<value>\".*\")$",c)
+        elif re.match("^var +[^ ]+ *= *.*$",c):
+            m = re.match("^var +(?P<varname>[^ ]+) *= *(?P<value>.*)$",c)
             value = do(m["value"])
             var[m["varname"]] = value
             return value
@@ -45,12 +52,14 @@ def do(script,**kargs):
                 fargs = []
                 for arg in str.split(m["args"],","):
                     fargs.append(do(arg))
-                return do(*fargs,execute=True)
+                v = do(*fargs,execute=True)
             else:
                 fargs = []
                 for arg in str.split(m["args"],","):
                     fargs.append(do(arg))
-                return func[fname](*fargs)
+                v = func[fname](*fargs)
+            if kargs.get("interpret",False) and not kargs.get("call",False): log(v)
+            return v
         elif re.match("^[^ \d][^ ]*$",c):
             try:
                 if kargs.get("interpret",False) and not kargs.get("call",False): print(var[c].raw)
