878ddcae16e4c590e99d9c9abf1c0e606da46939
Preta Crowz <pretacrowz@gmail.com>
2019-02-10 17:04:02 +0900

    added raw values

--- zclass.py
+++ zclass.py
@@ -10,39 +10,45 @@ class zbyte:
         self.value = value
         self._pyObject = value
         self._class = zbyte
+        self.raw = "\'"+str(value)+"\'"
 
 class zint:
     def __init__(self,value):
         self.value = value
         self._pyObject = value
         self._class = zint
+        self.raw = str(value)
 
 class zfloat:
     def __init__(self,value):
-        self.value = values
+        self.value = value
         self._pyObject = value
         self._class = zfloat
+        self.raw = value
 
 class zlist:
-    def __init__(self,*values):
-        self.value = values
-        self._pyObject = values
+    def __init__(self,*value):
+        self.value = list(value)
+        self._pyObject = list(value)
         self._class = zlist
+        self.raw = "["+",".join(self.value)+"]"
 
 class zset:
-    def __init__(self,*values):
-        self.value = values
-        self._pyObject = values
+    def __init__(self,*value):
+        self.value = set(value)
+        self._pyObject = set(value)
         self._class = zset
+        self.raw = "<"+",".join(self.value)+">"
 
 class ztuple:
-    def __init__(self,*values):
-        self.value = values
-        self._pyObject = values
+    def __init__(self,*value):
+        self.value = tuple(value)
+        self._pyObject = tuple(value)
         self._class = ztuple
+        self.raw = "("+",".join(self.value)+")"
 
 class zdict:
-    def __init__(self,*values):
-        self.value = values
-        self._pyObject = values
+    def __init__(self,*value):
+        self.value = dict(value)
+        self._pyObject = dict(value)
         self._class = zdict
